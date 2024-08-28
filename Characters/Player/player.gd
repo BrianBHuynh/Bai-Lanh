230a89cb9b97b65ec56345396dd54d442f21cffd
Brianvy <brian123huy@gmail.com>
2024-08-28 17:44:11 -0400

    Started first card
    Main character card created and research for dragging objects has begun

---
+++ Characters/Player/player.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@export var health = 100 #health amount of player
+@export var attack = 20 #attack value of the player
+@export var defense = 20 #defense of the player
+@export var pref_pos = "None" #prefered possition of the player
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
