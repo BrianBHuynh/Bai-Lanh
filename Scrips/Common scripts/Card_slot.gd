fe6097336fb9142a8204129a55365e038af2e4fd
Brianvy <brian123huy@gmail.com>
2024-08-29 00:10:57 -0400

    Basic card movement
    Allow the cards to move into the slots, will likely need a rewrite eventually

---
+++ Scrips/Common scripts/Card_slot.gd
@@ -0,0 +1,20 @@
+extends StaticBody2D
+
+var filled = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	modulate = Color(Color.ALICE_BLUE, .7)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if filled:
+		modulate = Color(Color.ALICE_BLUE, 0)
+		scale = Vector2(1,1)
+	else:
+		scale = Vector2(1,1)
+		if GlobalVars.dragging_card:
+			modulate = Color(Color.ALICE_BLUE, 1)
+		else:
+			modulate = Color(Color.ALICE_BLUE, .4)
