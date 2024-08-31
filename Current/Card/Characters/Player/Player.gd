b722afd37dc0f87919249bd4a1448713a1dc101e
Brianvy <brian123huy@gmail.com>
2024-08-31 00:30:44 -0400

    Refactoring - Card Script, minor adjustments to combat framework, created cardEngine
    Refactored code so that the functionality of cards is stored within cardEngine instead of within each individual code, makes the code easier to maintain over the long term and slightly easier to understand. Added position as a input for positional effect as to make it able to take in multiple positions for more flexibility through an array in case multiple positions are preferred and / or have different effects.

--- Current/Card/Characters/Player/Player.gd
+++ Current/Card/Characters/Player/Player.gd
@@ -6,7 +6,7 @@ var defense = 10 #Defense of the card
 var speed = 10 #Speed of the card
 
 #Position stats/effects should only be applied when the play button is pressed!
-var prefPos = "None" #Prefered possition of the card
+var prefPos = [] #Prefered possitions of the card
 var pos = "None" #Current position
 var posHealth = 0
 var posAttack = 0
@@ -30,101 +30,36 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if draggable: #Prevents alot of unecessary checks, uses select and unselect cards to make sure that the card is draggable
 		if Input.is_action_just_pressed("leftClick"):
-			pickupCard()
+			cards.pickupCard(self)
 		if Input.is_action_pressed("leftClick"):
-			holdCard()
+			cards.holdCard(self)
 		elif Input.is_action_just_released("leftClick"):
 			modulate = defaultColor;
 			globalVars.draggingCard = false
 			var tween = get_tree().create_tween()
 			if slotted > 0 && is_instance_valid(newSlot) && not newSlot.filled:
-				placeCard(tween)
+				cards.placeCard(self, tween)
 			else:
-				returnCard(tween)
+				cards.returnCard(self, tween)
 
+#For when the card enters a slto
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group('Slot'):
-		addSlot(body)
+		cards.addSlot(self, body)
 
+#For when the card leaves a slot
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group('Slot'):
-		removeSlot()
+		cards.removeSlot(self)
 
+#For when mouse enters the card
 func _on_mouse_entered() -> void:
-	selectCard()
+	cards.selectCard(self)
 
+#For when mouse leaves the card
 func _on_mouse_exited() -> void:
-	unselectCard()
+	cards.unselectCard(self)
 
-#Places the location of the Slot into slot ref and changes its color, incriments the slot int
-func addSlot(slot: Node2D):
-	slotted = slotted + 1
-	slot.modulate = Color(Color.SKY_BLUE, 2)
-	slot.scale = Vector2(1.1,1.1)
-	newSlot = slot
-
-#Decriments the slotted variable, then returns the slot back to it's default color
-func removeSlot():
-	slotted = slotted - 1
-	if slotted == 0:
-		scale = Vector2(1.1, 1.1)
-		modulate = Color(Color.ALICE_BLUE, 1)
-
-#Makes card draggable and shows that it is to player by changing color and size
-func selectCard():
-	if not globalVars.draggingCard:
-		draggable = true
-		scale = Vector2(1.1, 1.1)
-		modulate = Color(Color.ALICE_BLUE, 1)
-
-#Returns card back to normal
-func unselectCard():
-	if not globalVars.draggingCard:
-		draggable = false
-		modulate = defaultColor
-		scale = defaultSize
-
-#Moves card to front, calculates offset and initial position of card, sets dragging to be true
-func pickupCard():
-	offset = get_global_mouse_position() - global_position
-	initialPos = global_position
-	globalVars.draggingCard = true
-	modulate = Color(Color.LIGHT_GOLDENROD, 1.5);
-	move_to_front()
-
-#Updates card location
-func holdCard():
-	global_position = get_global_mouse_position() - offset
-
-#Moves card location to the slot's position, places card into the party, unfills the old slot if it exist, changes current slot to new slot and fills it
-func placeCard(tween: Tween):
-	tween.tween_property(self,"position",newSlot.position,0.2).set_ease(Tween.EASE_OUT)
-	if is_instance_valid(curSlot):
-		curSlot.filled = false
-	curSlot = newSlot
-	curSlot.filled = true
-	if not combat.playerParty.has(self):
-		combat.playerParty.append(self)
-
-#returns card back to old position when picking up
-func returnCard(tween: Tween):
-	tween.tween_property(self,"global_position",initialPos,0.2).set_ease(Tween.EASE_OUT)
-
-#Applies slot stats and effects
-func slotApply():
-	health = health + curSlot.slotHealth
-	attack = attack + curSlot.slotAttack
-	defense = defense + curSlot.slotDefense
-	speed = speed + curSlot.slotSpeed
-	curSlot.activate()
-
-func posApply():
-	if prefPos == curSlot.pos:
-		health = health + posHealth
-		attack = attack + posAttack
-		defense = defense + posDefense
-		speed = speed + posSpeed
-		posEffect()
-
-func posEffect():
+#For when the positional effect is activated for the card
+func posEffect(position):
 	pass
---
+++ Current/Scrips/Global scripts/cardEngine.gd
@@ -0,0 +1,86 @@
+extends Node2D
+
+func cardMove(card):
+	if card.draggable: #Prevents alot of unecessary checks, uses select and unselect cards to make sure that the card is draggable
+		if Input.is_action_just_pressed("leftClick"):
+			pickupCard(card)
+		if Input.is_action_pressed("leftClick"):
+			holdCard(card)
+		elif Input.is_action_just_released("leftClick"):
+			card.modulate = card.defaultColor;
+			globalVars.draggingCard = false
+			var tween = get_tree().create_tween()
+			if card.slotted > 0 && is_instance_valid(card.newSlot) && not card.newSlot.filled:
+				placeCard(card, tween)
+			else:
+				returnCard(card, tween)
+
+#Places the location of the Slot into slot ref and changes its color, incriments the slot int
+func addSlot(card, slot: Node2D):
+	card.slotted = card.slotted + 1
+	slot.modulate = Color(Color.SKY_BLUE, 2)
+	slot.scale = Vector2(1.1,1.1)
+	card.newSlot = slot
+
+#Decriments the slotted variable, then returns the slot back to it's default color
+func removeSlot(card):
+	card.slotted = card.slotted - 1
+	if card.slotted == 0:
+		scale = Vector2(1.1, 1.1)
+		modulate = Color(Color.ALICE_BLUE, 1)
+
+#Makes card draggable and shows that it is to player by changing color and size
+func selectCard(card):
+	if not globalVars.draggingCard:
+		card.draggable = true
+		card.scale = Vector2(1.1, 1.1)
+		card.modulate = Color(Color.ALICE_BLUE, 1)
+
+#Returns card back to normal
+func unselectCard(card):
+	if not globalVars.draggingCard:
+		card.draggable = false
+		card.modulate = card.defaultColor
+		card.scale = card.defaultSize
+
+#Moves card to front, calculates offset and initial position of card, sets dragging to be true
+func pickupCard(card):
+	card.offset = get_global_mouse_position() - card.global_position
+	card.initialPos = global_position
+	globalVars.draggingCard = true
+	card.modulate = Color(Color.LIGHT_GOLDENROD, 1.5);
+	card.move_to_front()
+
+#Updates card location
+func holdCard(card):
+	card.global_position = get_global_mouse_position() - card.offset
+
+#Moves card location to the slot's position, places card into the party, unfills the old slot if it exist, changes current slot to new slot and fills it
+func placeCard(card, tween: Tween):
+	tween.tween_property(card,"position",card.newSlot.position,0.2).set_ease(Tween.EASE_OUT)
+	if is_instance_valid(card.curSlot):
+		card.curSlot.filled = false
+	card.curSlot = card.newSlot
+	card.curSlot.filled = true
+	if not combat.playerParty.has(card):
+		combat.playerParty.append(card)
+
+#returns card back to old position when picking up
+func returnCard(card, tween: Tween):
+	tween.tween_property(card,"global_position",card.initialPos,0.2).set_ease(Tween.EASE_OUT)
+
+#Applies slot stats and effects
+func slotApply(card):
+	card.health = card.health + card.curSlot.slotHealth
+	card.attack = card.attack + card.curSlot.slotAttack
+	card.defense = card.defense + card.curSlot.slotDefense
+	card.speed = card.speed + card.curSlot.slotSpeed
+	card.curSlot.activate()
+
+func posApply(card):
+	if card.prefPos.has(card.curSlot.pos):
+		card.health = card.health + card.posHealth
+		card.attack = card.attack + card.posAttack
+		card.defense = card.defense + card.posDefense
+		card.speed = card.speed + card.posSpeed
+		card.posEffect(card.curSlot.pos)
