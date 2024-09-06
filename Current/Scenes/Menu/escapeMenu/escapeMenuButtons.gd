8ddecb30d77eb5748500dd22a5717bd3bd6e0b94
Brianvy <brian123huy@gmail.com>
2024-09-06 11:06:43 -0400

    Debug Menu added
    A very basic Debug menu was added with the options of moving forwards 10 turns, resetting the scene (and combat queue), going to the debug screen, and going to the small combat scene (unfinished)

---
+++ Current/Scenes/Menu/escapeMenu/escapeMenuButtons.gd
@@ -0,0 +1,27 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_button_3_pressed() -> void:
+	combat.clearData()
+	get_tree().reload_current_scene()
+
+
+func _on_button_4_pressed() -> void:
+	for i in 10:
+		combat.nextTurn()
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Current/Scenes/Combat/combatSmall.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Debug/Testing.tscn")
---
+++ Current/Scrips/Global scripts/Autoload/controls.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var menu
+var menuOpen = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	menu = preload("res://Current/Scenes/Menu/escapeMenu/escapeMenu.tscn").instantiate()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("escape"):
+		if not menuOpen:
+			get_tree().root.add_child(menu)
+			menuOpen = true
+		else:
+			get_tree().root.remove_child(menu)
+			menuOpen = false
