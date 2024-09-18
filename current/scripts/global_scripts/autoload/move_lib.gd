ec6cab09cd70f71bc5af22f883847467337562a0
Brianvy <brian123huy@gmail.com>
2024-09-18 11:43:15 -0400

    Hotfix
    Hotfix for incorrect static typing

--- current/scripts/global_scripts/autoload/move_lib.gd
+++ current/scripts/global_scripts/autoload/move_lib.gd
@@ -29,12 +29,12 @@ func move_then_return(card: Card, destination: Vector2) -> void:
 		tween = get_tree().create_tween()
 		tween.tween_property(card,"position",card.current_position,0.2).set_ease(Tween.EASE_OUT)
 
-func change_scale(card: Card, size: Vector2) -> void:
+func change_scale(card: Node2D, size: Vector2) -> void:
 	if is_instance_valid(card):
 		tween = get_tree().create_tween()
 		tween.tween_property(card,"scale",size,0.1).set_ease(Tween.EASE_OUT)
 
-func change_color(card: Card, color: Color) -> void:
+func change_color(card: Node2D, color: Color) -> void:
 	if is_instance_valid(card):
 		tween = get_tree().create_tween()
 		tween.tween_property(card,"modulate",color,0.15).set_ease(Tween.EASE_OUT)
