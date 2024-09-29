e225ce47fc0b03eab8be73d6a8d4d2432883eb56
Brianvy <brian123huy@gmail.com>
2024-09-29 02:30:12 -0400

    Moved files around, preparation for shaders
    Started experimenting with shaders for animations, progress is slow but damage animations and death animations are needed

--- current/scenes/menu/escape_menu/escape_menu_buttons.gd
+++ current/scenes/menu/escape_menu/escape_menu_buttons.gd
@@ -8,7 +8,6 @@ var ticking = false
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if ticking and current_turns < fast_forward_turns:
