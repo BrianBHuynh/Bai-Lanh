2a2d8674ae62a5b8309fc3630dafb7bda207c4bc
Brianvy <brian123huy@gmail.com>
2024-10-18 23:39:12 -0400

    Save encryption + redundancy + checksums
    Currently I am expanding the save system to have some encryption connected to the unique id of the system it is played on preventing the moving of saves between players. Along side this each save comes with a checksum and redundant backup saves in case of corruption.
    
    As players likely will have multiple computers, as well as possibly may want to share saves with other players, I will add a in game way to unbind saves eventually, however at the same time I'll raise a flag on first time loading said save that it is from another computer and that you should only use said save if you trust the source it came from.
    
    This + the way I formatted the json file should lower the chances of arbitrary code execution on the side of users, and warn them if they decide to risk using someone elses save.
    
    The checksums will also warn users if the save was modified, however if they wish to use a modified save, it will likely be used to disable achievements or something else of the sorts.
    
    Preventing players from being able to modify their saves I think is a useless and futile task, however I think that encouraging honest play as well as improving the player's security is a doable task, and giving them the tools to circumvent it easily and accessibly is important as long as they're aware of the risk.

--- current/scripts/global_scripts/autoload/game_saves.gd
+++ current/scripts/global_scripts/autoload/game_saves.gd
@@ -1,15 +1,38 @@
 extends Node
 
+func _ready() -> void:
+	if not DirAccess.dir_exists_absolute("user://saves"):
+		DirAccess.make_dir_absolute("user://saves")
+	if not DirAccess.dir_exists_absolute("user://backup"):
+		DirAccess.make_dir_absolute("user://backup")
+	if not DirAccess.dir_exists_absolute("user://fallback"):
+		DirAccess.make_dir_absolute("user://fallback")
+
 func save_file(content, location: String):
-	var file = FileAccess.open_encrypted_with_pass("user://" + location + ".lanh_shard", FileAccess.WRITE, GlobalVars.player_name)
 	var content_json = JSON.stringify(content)
-	file.store_var(content_json, true)
+	OpenWrite("user://saves/" + location + ".lanhfile").store_var(content_json, false)
+	OpenWrite("user://saves/" + location + ".checksum").store_var(FileAccess.get_sha256("user://saves/" + location + ".lanhshard"), false)
+	OpenWrite("user://backup/" + location + ".lanhshard").store_var(content_json, false)
+	OpenWrite("user://backup/" + location + ".checksum").store_var(FileAccess.get_sha256("user://backup/" + location + ".lanhshard"), false)
+	OpenWrite("user://fallback/" + location + ".lanhshard").store_var(content_json, false)
+	OpenWrite("user://fallback/" + location + ".checksum").store_var(FileAccess.get_sha256("user://fallback/" + location + ".lanhshard"), false)
 
 func load_file(location):
-	var file = FileAccess.open_encrypted_with_pass("user://" + location + ".lanh_shard", FileAccess.READ, GlobalVars.player_name)
-	if is_instance_valid(file):
-		var content: Dictionary = JSON.parse_string(file.get_var(true))
-		return content
+	var file = FileAccess.open_encrypted_with_pass("user://saves/" + location + ".lanhshard", FileAccess.READ, OS.get_unique_id())
+	print("test here")
+	print(FileAccess.get_sha256("user://saves/" + location + ".lanhshard"))
+	print(FileAccess.open_encrypted_with_pass("user://saves/" + location + ".checksum", FileAccess.READ, OS.get_unique_id()).get_var())
+	if is_instance_valid(file) and FileAccess.get_sha256("user://saves/" + location + ".lanhshard") == FileAccess.open_encrypted_with_pass("user://saves/" + location + ".checksum", FileAccess.READ, OS.get_unique_id()).get_var():
+		print("Checksum 1 passed")
+		var content = JSON.new()
+		content.parse(file.get_var(true), false)
+		return content.data
 
 func save_game():
 	pass
+
+func OpenWrite(path: String) -> FileAccess:
+	return FileAccess.open_encrypted_with_pass(path, FileAccess.WRITE, OS.get_unique_id())
+
+func OpenRead(path: String) -> FileAccess:
+	return FileAccess.open_encrypted_with_pass(path, FileAccess.READ, OS.get_unique_id())
